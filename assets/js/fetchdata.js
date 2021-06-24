b3890b69cc5ecb3802dee1ee9440af7c559909e7
Sahil Bhatiya <64317865+SahilBhatiya@users.noreply.github.com>
2021-06-24 22:11:06 +0530

    Added Basic Function

---
+++ assets/js/fetchdata.js
@@ -0,0 +1,17 @@
+(() => {
+    firebase.database().ref("Customers").once('value', function(snapshot) {
+        let k = 1;
+        snapshot.forEach(function(ChildSnapshot) {
+            document.body.innerHTML += "<hr>";
+            ChildSnapshot.forEach(function(GrandChildSnapshot) {
+                const val = GrandChildSnapshot.val();
+                document.body.innerHTML += "ID : " + GrandChildSnapshot.val().ID + "<br>";
+                for (const [key, value] of Object.entries(val)) {
+                    document.body.innerHTML += `${key}: ${value} <br>`;
+                }
+
+            })
+            document.body.innerHTML += "<br>";
+        });
+    });
+})();
---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAsmgrx8pOkky6vEQJZJYPdhCS2U7Eheuk",
+    authDomain: "adarsh-wheel-alignment.firebaseapp.com",
+    databaseURL: "https://adarsh-wheel-alignment-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "adarsh-wheel-alignment",
+    storageBucket: "adarsh-wheel-alignment.appspot.com",
+    messagingSenderId: "1085995154494",
+    appId: "1:1085995154494:web:3bd367766b1a9a9d10656f",
+    measurementId: "G-18H00ZC8Y7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
