01e5e63cc5b570192eeaddc7414cbf82c22f6b4c
Sahil Bhatiya <64317865+SahilBhatiya@users.noreply.github.com>
2021-06-28 13:59:24 +0530

    Added Tracking

--- assets/js/Auth.js
+++ assets/js/Auth.js
@@ -6,7 +6,7 @@ async function Login() {
             var user = userCredential.user;
         })
         .catch((error) => {
-            DisplayMsg(1, "Error!", "Cannot Login", 0, "t");
+            DisplayMsg(1, "Sorry!", "You Are Not Registered", 0, "t");
         });
 
 }
@@ -15,6 +15,6 @@ firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         document.body.classList.remove("d-none");
     } else if (window.location.includes("Dashboard")) {
-        DisplayMsg(1, "Error!", "Cannot Access", 0, "t");
+        DisplayMsg(1, "Sorry!", "You Are Not Registered", 0, "t");
     }
 });
